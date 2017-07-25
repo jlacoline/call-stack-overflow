2c9d65d08c7f37a58754532736884df562d75fa9
jlacoline <jean.lacoline@gmail.com>
2017-07-25 19:26:41 +0200

    feature: try to find python docs urls in answers and call linked element

--- callstackoverflow/__init__.py
+++ callstackoverflow/__init__.py
@@ -3,6 +3,7 @@ import requests
 import html
 import re
 import logging
+import itertools
 
 from google import search
 
@@ -17,6 +18,9 @@ RE_ANSWER = re.compile(r'<div id="answer-.*?</table', re.DOTALL)
 RE_CODE = re.compile(
     r"<pre[^>]*>[^<]*<code[^>]*>((?:\s|[^<]|<span[^>]*>[^<]+</span>)*)"
     r"</code></pre>")
+RE_DOC_URL = re.compile(
+    r"<a href=([\"\']"
+    r"https://docs\.python\.org/(?:\d/)?library/(.*).html#([^\"^\']*))[\"\']")
 
 
 # Credits to Filip Haglund for stackoverflow html parsing
@@ -28,6 +32,23 @@ def _find_code_in_html(s):
         codez = map(lambda x: x.group(1), codez)
         for code in sorted(codez, key=lambda x: -len(x)):
             code = html.unescape(code)
+            yield code
+
+
+def _find_doc_url_in_html_and_make_code(raw_url):
+    answers = re.findall(RE_ANSWER, raw_url)
+    for answer in answers:
+        for match in re.finditer(RE_DOC_URL, answer):
+            lib, func = match.group(2), match.group(3)
+            code = """
+try:
+    import {}
+except Exception:
+    pass
+
+def code_from_python_doc(*args, **kwargs):
+    return {}(*args, **kwargs)
+            """.format(lib, func)
             yield code
 
 
@@ -70,6 +91,7 @@ def _make_function_from_shell_script(code, name):
 def get_function(query, test_func=None, func_names=None):
     if not func_names:
         func_names = _generate_potential_names_from_query(query)
+    func_names.append("code_from_python_doc")  # super ugly, refactor asap
 
     # search on google
     links = search("site:stackoverflow.com python {}".format(query), stop=1)
@@ -78,7 +100,9 @@ def get_function(query, test_func=None, func_names=None):
     for link in links:
         logger.debug("Trying %s", link)
         raw_html = requests.get(link).text
-        for code in _find_code_in_html(raw_html):
+        for code in itertools.chain(
+                _find_doc_url_in_html_and_make_code(raw_html),
+                _find_code_in_html(raw_html)):
             if code.startswith(">>>"):
                 code = _make_function_from_shell_script(code, func_names[0])
             for func in _search_for_def_keyword(func_names, code):
