647644773533f8bb4c964eb18256d4f4a15d01fc
jlacoline <jean.lacoline@gmail.com>
2017-07-25 13:37:26 +0200

    setup.py: fix typo in author email

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='Just let stackoverflow.com do your job',
     url='https://github.com/jlacoline/call-stack-overflow',
     author='https://github.com/jlacoline',
-    author_email='jean.lacoline@.com',
+    author_email='jean.lacoline@gmail.com',
     license='WTFPL',
     keywords='stackoverflow',
     packages=["callstackoverflow"],
